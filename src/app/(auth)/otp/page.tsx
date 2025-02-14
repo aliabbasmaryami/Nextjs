510c4eff7e0b75bbf33eaef078935877eccf19c1
React <aliabbasmaryami50@gmail.com>
2025-02-14 19:23:33 +0500

    buttons adding

--- src/app/(auth)/otp/page.tsx
+++ src/app/(auth)/otp/page.tsx
@@ -2,7 +2,7 @@ export default function Otp(){
     return(
         <div>
             <h3>Otp screen</h3>
-            <p>Enter Opt send </p>
+
             <button>send</button>
         </div>
     )
