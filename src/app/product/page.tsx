0dcb649507f1b8239e6435cdb8d6f40c9e8b92c5
React <aliabbasmaryami50@gmail.com>
2025-03-12 18:03:35 +0500

    product b3

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,7 +5,7 @@ export default function (){
             Product list
         </h1>
         <h2>This is product 1</h2>
-        <h2>product 2</h2>
+        <h2>This is product 2</h2>
         <h2>product 3</h2>
         <h2>product 4</h2>
         <h2>product 5</h2>
