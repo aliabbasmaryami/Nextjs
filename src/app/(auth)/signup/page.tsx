38ace7061a60d3b54db56fe675920c34edb09037
React <aliabbasmaryami50@gmail.com>
2025-03-22 19:00:34 +0500

    updates

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ export default function Signup(){
     return(
         <div>
             <h3>This is SignUp page </h3>
-            <p>Hello This is Sign Up page</p>
+            <p>Hello This is Sign Up page and this is for Testing</p>
         </div>
     )
 }
