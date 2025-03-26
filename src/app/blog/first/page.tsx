62b19a2202d614e9ef1df734e0bcd16aa4db3ba6
React <aliabbasmaryami50@gmail.com>
2025-03-26 19:02:13 +0500

    changed layout

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,7 @@
-export default function (){
-    return <h3>This is firts blog page </h3>
+export default function () {
+    return (
+        <div><h3>This is firts blog page </h3>
+        <p>this is for testing</p>
+        </div>
+    )
 }
