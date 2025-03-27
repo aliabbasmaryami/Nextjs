1054757aa5e745644828d4e59721abd83c164362
React <aliabbasmaryami50@gmail.com>
2025-03-27 19:03:19 +0500

    page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound(){
     return(
         <div>
             <h1>page not found</h1>
-            <p>Could not find requested </p>
+            <p>This Means the source Not found</p>
         </div>
     )
 }
