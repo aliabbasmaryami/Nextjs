db8f4b79c42e7dcf8ac59aa26143b04a7922b977
React <aliabbasmaryami50@gmail.com>
2025-04-18 17:53:20 +0500

    pageNotfound in next js

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound(){
     return(
         <div>
             <h1>page not found</h1>
-            <p>could not find requested resource</p>
+            <p>Could not find requested </p>
         </div>
     )
 }
