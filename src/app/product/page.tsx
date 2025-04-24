abac214f23a37773c2975c93ff5208e3323145cb
React <aliabbasmaryami50@gmail.com>
2025-04-24 14:24:20 +0500

    changes

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -7,6 +7,8 @@ export default function (){
         <h2>product 1</h2>
         <h2>product 2</h2>
         <h2>product 3</h2>
+        <h2>product 4</h2>
+        <h2>product 5</h2>
         </>
     )
 }
